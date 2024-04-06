ce4ae2d31922c0c60bcd85f3f8e326fe6b93b6ce
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-04-06 11:46:12 +0530

    Add comment

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -107,7 +107,7 @@ DATABASES = {
 }
 
 import dj_database_url
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config() #postgresql for heroku
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
 
