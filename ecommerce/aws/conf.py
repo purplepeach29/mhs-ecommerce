49105707b917ac662c567d0bd79ccaf02cf072a7
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-04-14 23:01:38 +0530

    Remove secret key

--- ecommerce/aws/conf.py
+++ ecommerce/aws/conf.py
@@ -3,8 +3,6 @@ import datetime
 AWS_GROUP_NAME = "mhs_eCommerce_Group"
 AWS_USERNAME = "mhs-ecommerce-user"
 
-AWS_ACCESS_KEY_ID = "AKIAU6GDZ7WU4JRN6EV7"
-AWS_SECRET_ACCESS_KEY = "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"
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
@@ -13,6 +11,7 @@ DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'mhs-ecommerce'
 S3DIRECT_REGION = 'ap-south-1'
+AWS_S3_SIGNATURE_VERSION = "s3v4" #Prachiadded
 S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_ROOT = MEDIA_URL
